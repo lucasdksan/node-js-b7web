68acc54122e726100b88cd81342e0eb9e6cfcc21
lucasdksan <lucas.leoncio.silva@gmail.com>
2024-12-08 12:20:18 -0300

    feat: fim do curso

--- secret-friend/src/utils/requestIntercepter.ts
+++
@@ -1,6 +0,0 @@
-import { RequestHandler } from "express";
-
-export const requestIntercepter: RequestHandler = (req, res, next) => {
-    console.log(`-> ${res.statusCode} ${req.method} ${req.originalUrl} ${JSON.stringify(req.body)}`);
-    next();
-}
